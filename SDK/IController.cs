d957e00d8fa6650a75d9e99a8319c554925269ad
Hactazia <hactazia@gmail.com>
2026-05-31 22:36:21 +0200

    chore: remove unused using directive

--- SDK/IController.cs
+++ SDK/IController.cs
@@ -2,7 +2,6 @@ using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
 using Nox.CCK.Players;
 using Nox.CCK.Utils;
-using Nox.Players;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
